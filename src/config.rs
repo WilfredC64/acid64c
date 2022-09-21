5107290f95eace81270deb4b4ee8707420276a64
WilfredC64 <wilfred.bos@xs4all.nl>
2022-09-21 19:55:52 +0200

    moved argument processing to config struct

---
+++ src/config.rs
@@ -0,0 +1,78 @@
+// Copyright (C) 2022 Wilfred Bos
+// Licensed under the GNU GPL v3 license. See the LICENSE file for the terms and conditions.
+
+use std::env;
+
+pub struct Config {
+    pub hvsc_location: Option<String>,
+    pub host_name: Option<String>,
+    pub display_stil: bool,
+    pub display_devices: bool,
+    pub adjust_clock: bool,
+    pub device_numbers: Vec<i32>,
+    pub song_number: i32,
+    pub filename: String
+}
+
+impl Config {
+    pub fn read() -> Result<Config, String> {
+        let mut hvsc_location = None;
+        let mut host_name = None;
+        let mut display_stil = false;
+        let mut display_devices = false;
+        let mut adjust_clock = false;
+        let mut device_numbers = vec![-1];
+        let mut song_number = -1;
+        let filename = env::args().last().unwrap();
+
+        for argument in env::args().filter(|arg| arg.len() > 1 && arg.starts_with('-')) {
+            match &argument[1..2] {
+                "c" => adjust_clock = true,
+                "d" => device_numbers = Self::parse_argument_numbers("Device number", &argument[2..])?,
+                "h" => host_name = Some(argument[2..].to_string()),
+                "i" => display_stil = true,
+                "l" => hvsc_location = Some(argument[2..].to_string()),
+                "p" => display_devices = true,
+                "s" => song_number = Self::parse_argument_number("Song number", &argument[2..])?,
+                _ => return Err(format!("Unknown option: {}", argument))
+            }
+        }
+
+        Ok(Config {
+            hvsc_location,
+            host_name,
+            display_stil,
+            display_devices,
+            adjust_clock,
+            device_numbers,
+            song_number,
+            filename
+        })
+    }
+
+    fn parse_argument_numbers(arg_name: &str, arg_values: &str) -> Result<Vec<i32>, String> {
+        let values = arg_values.split(',');
+        let mut numbers = vec![];
+        for value in values {
+            let result = Self::parse_argument_number(arg_name, value);
+            if result.is_err() {
+                return Err(result.err().unwrap());
+            }
+            numbers.push(result.unwrap());
+        }
+        Ok(numbers)
+    }
+
+    fn parse_argument_number(arg_name: &str, arg_value: &str) -> Result<i32, String> {
+        let number = match arg_value.parse::<i32>() {
+            Ok(i) => i,
+            Err(_e) => return Err(format!("{} must be a valid number and must be higher than 0.", arg_name))
+        };
+
+        if number >= 1 {
+            Ok(number - 1)
+        } else {
+            Err(format!("{} must be higher than 0.", arg_name))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 // Copyright (C) 2019 - 2022 Wilfred Bos
 // Licensed under the GNU GPL v3 license. See the LICENSE file for the terms and conditions.
 
+mod config;
 mod console_player;
 mod player;
 mod utils;
 
 use std::env;
 use std::process::exit;
+use self::config::Config;
 use self::console_player::ConsolePlayer;
 use self::player::Player;
 
 fn main() {
+    if env::args().count() <= 1 {
+        print_usage();
+        return;
+    }
+
     match run() {
         Ok(_) => {}
         Err(message) => {
@@ -20,90 +27,41 @@ fn main() {
     }
 }
 
-fn parse_argument_numbers(arg_name: &str, arg_values: &str) -> Result<Vec<i32>, String> {
-    let values = arg_values.split(',');
-    let mut numbers = vec![];
-    for value in values {
-        let result = parse_argument_number(arg_name, value);
-        if result.is_err() {
-            return Err(result.err().unwrap());
-        }
-        numbers.push(result.unwrap());
-    }
-    Ok(numbers)
-}
-
-fn parse_argument_number(arg_name: &str, arg_value: &str) -> Result<i32, String> {
-    let number = match arg_value.parse::<i32>() {
-        Ok(i) => i,
-        Err(_e) => {
-            return Err(format!("{} must be a valid number and must be higher than 0.", arg_name));
-        }
-    };
-
-    if number >= 1 {
-        Ok(number - 1)
-    } else {
-        Err(format!("{} must be higher than 0.", arg_name))
-    }
-}
-
 fn run() -> Result<(), String> {
-    if env::args().count() <= 1 {
-        print_usage();
-        return Ok(());
-    }
-
-    let mut hvsc_location = None;
-    let mut display_stil = false;
-    let mut display_devices = false;
-    let mut device_numbers = vec![-1];
-    let mut song_number = -1;
-    let filename = env::args().last().unwrap();
-
     let mut player = Player::new();
-    let device_names = player.get_device_names();
+    let config = Config::read()?;
 
-    for argument in env::args().filter(|arg| arg.len() > 1 && arg.starts_with('-')) {
-        match &argument[1..2] {
-            "c" => {
-                player.set_adjust_clock(true);
-            },
-            "d" => device_numbers = parse_argument_numbers("Device number", &argument[2..])?,
-            "h" => {
-                let host_name = argument.chars().skip(2).collect();
-                player.set_host_name(host_name);
-            },
-            "i" => display_stil = true,
-            "l" => hvsc_location = Some(argument.chars().skip(2).collect()),
-            "p" => display_devices = true,
-            "s" => song_number = parse_argument_number("Song number", &argument[2..])?,
-            _ => ()
-        }
+    if config.adjust_clock {
+        player.set_adjust_clock(true);
     }
 
-    player.set_device_numbers(device_numbers);
+    if let Some(host_name) = config.host_name {
+        player.set_host_name(host_name);
+    }
+
+    player.set_device_numbers(config.device_numbers);
     player.init_devices()?;
 
-    if display_devices {
+    if config.display_devices {
+        let device_names = player.get_device_names();
         print_device_names(device_names.lock().unwrap().to_vec());
         return Ok(());
     }
 
-    player.load_file(&filename)?;
-    if song_number != -1 {
-        player.set_song_to_play(song_number)?;
+    player.load_file(&config.filename)?;
+    if config.song_number != -1 {
+        player.set_song_to_play(config.song_number)?;
     }
-    player.setup_sldb_and_stil(hvsc_location, display_stil)?;
+    player.setup_sldb_and_stil(config.hvsc_location, config.display_stil)?;
 
     let version = player.get_library_version();
     if version < 0x210 {
         return Err("acid64pro.dll version 2.1.0 or higher required.".to_string());
     }
 
-    println!("ACID64 library version v{}.{}.{}", version >> 8, version >> 4 & 0x0f, version & 0x0f);
+    print_library_version(version);
 
-    let mut console_player = ConsolePlayer::new(player, display_stil);
+    let mut console_player = ConsolePlayer::new(player, config.display_stil);
     console_player.play()?;
     Ok(())
 }
@@ -130,4 +88,8 @@ fn print_device_names(device_names: Vec<String>) {
     } else {
         println!("No devices were found.");
     }
+}
+
+fn print_library_version(version: i32) {
+    println!("ACID64 library version v{}.{}.{}", version >> 8, version >> 4 & 0x0f, version & 0x0f);
 }
