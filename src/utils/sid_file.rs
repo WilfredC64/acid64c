c91d3ed9ea16257b4d5547944a44e48ce6581e5b
WilfredC64 <wilfred.bos@xs4all.nl>
2023-11-22 19:38:09 +0100

    simplified

--- src/utils/sid_file.rs
+++ src/utils/sid_file.rs
@@ -5,6 +5,5 @@
 pub const MIN_SID_HEADER_SIZE: usize = 0x76;
 
 pub fn is_sid_file(source: &[u8]) -> bool {
-    source.len() >= MIN_SID_HEADER_SIZE &&
-        (source[0] == b'R' || source[0] == b'P') && source[1] == b'S' && source[2] == b'I' && source[3] == b'D'
+    source.len() >= MIN_SID_HEADER_SIZE && matches!(&source[0..4], b"RSID" | b"PSID")
 }
